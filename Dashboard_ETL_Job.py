630978b6b4183477f78d1b0f86581e2c4dfc43f9
chi19961026 <118462415+chi19961026@users.noreply.github.com>
2024-08-08 09:29:18 +0800

    Update ETL Process
    Transform a SQL output format 'DynamicFrame' to an S3-acceptable format as a 'DynamicFrameCollection'.

--- Dashboard_ETL_Job.py
+++ Dashboard_ETL_Job.py
@@ -4,8 +4,14 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrameCollection
+from awsglue.dynamicframe import DynamicFrame
 from awsglue import DynamicFrame
 
+# Script generated for node Custom Transform
+def MyTransform(glueContext, dfc) -> DynamicFrameCollection:
+    new_dfc = dfc.select(list(dfc.keys())[0]).repartition(1)
+    return(DynamicFrameCollection({"CustomTransform0": new_dfc}, glueContext))
 def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
     for alias, frame in mapping.items():
         frame.toDF().createOrReplaceTempView(alias)
@@ -43,7 +49,7 @@ OrderChangeSchema_node1722540319097 = ApplyMapping.apply(frame=Order_node1722540
 ChangeSchema_node1722917021605 = ApplyMapping.apply(frame=Location_node1722916992929, mappings=[("postal code", "string", "postal_code", "string"), ("city", "string", "city", "string"), ("state", "string", "state", "string"), ("region", "string", "region", "string")], transformation_ctx="ChangeSchema_node1722917021605")
 
 # Script generated for node SQL Query
-SqlQuery590 = '''
+SqlQuery811 = '''
 WITH first_order AS (SELECT customer_id, order_date,
     ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date ASC) AS rank
 FROM o)
@@ -53,18 +59,24 @@ FROM c LEFT JOIN first_order fo
 ON c.customer_id = fo.customer_id
 WHERE fo.rank = 1
 '''
-SQLQuery_node1722546169017 = sparkSqlQuery(glueContext, query = SqlQuery590, mapping = {"o":OrderChangeSchema_node1722540319097, "c":CustomerChangeSchema_node1722546110782}, transformation_ctx = "SQLQuery_node1722546169017")
+SQLQuery_node1722546169017 = sparkSqlQuery(glueContext, query = SqlQuery811, mapping = {"o":OrderChangeSchema_node1722540319097, "c":CustomerChangeSchema_node1722546110782}, transformation_ctx = "SQLQuery_node1722546169017")
 
-# Script generated for node Amazon S3
-AmazonS3_node1722916975856 = glueContext.write_dynamic_frame.from_options(frame=ProductChangeSchema_node1722916954168, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1722916975856")
+# Script generated for node Custom Transform
+CustomTransform_node1723080137686 = MyTransform(glueContext, DynamicFrameCollection({"SQLQuery_node1722546169017": SQLQuery_node1722546169017}, glueContext))
+
+# Script generated for node Select From Collection
+SelectFromCollection_node1723080159757 = SelectFromCollection.apply(dfc=CustomTransform_node1723080137686, key=list(CustomTransform_node1723080137686.keys())[0], transformation_ctx="SelectFromCollection_node1723080159757")
+
+# Script generated for node Product_etl
+Product_etl_node1722916975856 = glueContext.write_dynamic_frame.from_options(frame=ProductChangeSchema_node1722916954168, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Product_etl_node1722916975856")
 
 # Script generated for node Order_etl
 Order_etl_node1722916883837 = glueContext.write_dynamic_frame.from_options(frame=OrderChangeSchema_node1722540319097, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Order_etl_node1722916883837")
 
-# Script generated for node Amazon S3
-AmazonS3_node1722917033018 = glueContext.write_dynamic_frame.from_options(frame=ChangeSchema_node1722917021605, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1722917033018")
+# Script generated for node Location_etl
+Location_etl_node1722917033018 = glueContext.write_dynamic_frame.from_options(frame=ChangeSchema_node1722917021605, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Location_etl_node1722917033018")
 
 # Script generated for node Customer_etl
-Customer_etl_node1722727550044 = glueContext.write_dynamic_frame.from_options(frame=SQLQuery_node1722546169017, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Customer_etl_node1722727550044")
+Customer_etl_node1722727550044 = glueContext.write_dynamic_frame.from_options(frame=SelectFromCollection_node1723080159757, connection_type="s3", format="glueparquet", connection_options={"path": "s3://sales-customer-dashboard/output_data/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="Customer_etl_node1722727550044")
 
 job.commit()
